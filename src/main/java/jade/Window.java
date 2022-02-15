287cbc6ed08d5108fb5bbd64e80a73ab40ee5cb5
Rich Hoyt <rhoyt10@gmail.com>
2022-02-15 12:08:13 -0500

    Add Scene classes

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -15,10 +15,12 @@ public class Window {
     private String title;
     private long glfwWindow;
 
-    private float r, g, b, a;
+    public float r, g, b, a;
     private boolean fadeToBlack = false;
 
     private static Window window = null;
+
+    private static Scene currentScene;
 
     private Window() {
         this.width = 1920;
@@ -28,6 +30,21 @@ public class Window {
         b = 1;
         g = 1;
         a = 1;
+    }
+
+    public static void changeScene(int newScene) {
+        switch (newScene) {
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                break;
+            default:
+                assert false : "Unkown scene '" + newScene + "'";
+                break;
+        }
     }
 
     public static Window get() {
@@ -91,12 +108,13 @@ public class Window {
         creates the GLCapabilities instance and makes the OPen GL
         bindings available for use. */
         GL.createCapabilities();
-
+        Window.changeScene(0);
     }
 
     public void loop() {
         float beginTime = Time.getTime();
-        float endTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
 
         while (!glfwWindowShouldClose(glfwWindow)) {
             // Poll events (key events, mouse events etc.)
@@ -104,22 +122,25 @@ public class Window {
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if (fadeToBlack) {
-                r = Math.max(r - 0.01f, 0);
-                g = Math.max(g - 0.01f, 0);
-                b = Math.max(b - 0.01f, 0);
+            if (dt >= 0) {
+                currentScene.update(dt);
             }
 
-            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
-                fadeToBlack = true;
-
-            }
+//            if (fadeToBlack) {
+//                r = Math.max(r - 0.01f, 0);
+//                g = Math.max(g - 0.01f, 0);
+//                b = Math.max(b - 0.01f, 0);
+//            }
+//
+//            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+//                fadeToBlack = true;
+//            }
 
             glfwSwapBuffers(glfwWindow);
 
             endTime = Time.getTime();
             // dt as in Delta Time
-            float dt = endTime - beginTime;
+            dt = endTime - beginTime;
             beginTime = endTime;
         }
     }
